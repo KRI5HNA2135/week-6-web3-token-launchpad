babfb3e80046a2e17537b7543cca34db1aa947f5
Krushna Raut <krishraut2103@gmail.com>
2025-07-02 10:51:58 +0530

    commit - 4

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -13,8 +13,27 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 function App() {
   return (
     
+  <div>
+    <ConnectionProvider endpoint ={"https://api.devnet.solana.com"} >
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+         
+         <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+           <WalletMultiButton />
+          <WalletDisconnectButton />
+
+         </div>
+
     <TokenLaunchpad></TokenLaunchpad>
           
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  </div>          
 
   )
 }
